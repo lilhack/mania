744abc6d4695193af284af897d1caec3c64b98ec
Lucy Newman <newmanlucy@uchicago.edu>
2018-02-10 17:33:27 -0600

    hi

--- Python/src/message.py
+++ Python/src/message.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 
-from user import User
-
 class Message(object):
 	"""docstring for Message"""
-	def __init__(self, sender, text):
+	def __init__(self, text):
 		self.sender = sender
 		self.text = text
 
 	def __repr__(self):
-		string = "%s says: %s \nI'm at %s" % \
-		 (self.sender.name, self.text, self.sender.getAddress())
+		string = "%s\nI'm at %s" % \
+		 (self.text, self.sender.getAddress())
 		return string
 
 if __name__ == '__main__':
-	user1 = User("choyin1", "cho yin", "/none", ("52.509669", "13.376294"), "000-000-0000")
-	msg = Message(user1, "I need some help")
+	msg = Message("I need some help")
 	print(msg)
--- Python/src/user.py
+++ Python/src/user.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 from geopy.geocoders import Nominatim
 from geopy.distance import vincenty
+import smtplib
+from email.message import EmailMessage
+from message import Message
 
 class User(object):
 
-    def __init__(self, username, name, imgURL, location, phone, contacts, text="I need some help"):
+    def __init__(self, username, name, imgURL, location, phone, provider, contacts, text="I need some help"):
         self.username = username
         self.name = name
         self.imgURL = imgURL
@@ -12,6 +15,7 @@ class User(object):
         self.phone = phone
         self.text = text
         self.contacts = contacts # list of Users
+        self.provider = provider
 
     def __repr__(self):
         string = "User: %s" % self.username
@@ -19,9 +23,26 @@ class User(object):
         string += "\n\tlocation: (%s, %s)" % (self.location[0], self.location[1])
         string += "\n\tphone: %s" % self.phone
         string += "\n\ttext: %s" % self.text
-        string += ("***\nCONTACTS:\n***\n")
+        string += "\n\tmessage: %s" % self.message
+        string += "\n\temail: %s" % self.email
+        string += ("\n***\nCONTACTS:\n***\n")
         for c in self.contacts:
             string += str(c)
+        return string
+
+    @property
+    def email(self):
+        if self.provider == "AT&T":
+            return self.phone + "@mms.att.net"
+        elif self.provider == "Verizon":
+            pass
+            # TODO
+        return None
+
+    @property    
+    def message(self):
+        string = "%s\nI'm at %s" % \
+         (self.text, self.getAddress())
         return string
 
     def getAddress(self):
@@ -32,20 +53,26 @@ class User(object):
     def getDistance(self, other):
         return vincenty(self.location, other.location)
 
-    def sendMessage(self, recipient):
-        message = Message(self, self.text)
-        recipient.notify(message)
 
+    def sendMessage(self, recipient):
+        msg = EmailMessage()
+        msg.set_content(self.message)
+        msg['From'] = "lucynoodles20@gmail.com"
+        msg['To'] = recipient.email
+
+        with smtplib.SMTP('localhost') as s:
+            s.send_message(msg)
+    
     def addContact(self, contact):
         self.contacts.append(contact)
 
-    def notify(self, message):
-        pass
 
 if __name__ == '__main__':
-    user1 = User("choyin1", "cho yin", "/none", ("52.509669", "13.376294"), "0000000000", [])
-    print(user1.getAddress())
-    user2 = User("choyin2", "cho yin", "/none", ("51.509669", "13.376294"), "0000000000", [], text="pls help")
-    print(user1.getDistance(user2))
-    user2.addContact(user1)
-    print(user2)
+    user1 = User("angela1", "Angela", "/none", \
+        ("52.509669", "13.376294"), "7135347983",\
+         "AT&T", [])
+    user2 = User("choyin2", "cho yin", "/none", \
+        ("51.509669", "13.376294"), "0000000000", \
+        "Verizon", [], text="pls help")
+    print(user1)
+    user2.sendMessage(user1)
